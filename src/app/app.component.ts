cb8df79541f58801465520b63179db10b1b939fa
jimas13 <machetecontrolande@hotmail.com>
2017-12-28 07:56:17 +0200

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, animate, trigger,  transition, group, style, state } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('moveSidebar', [
+      state('collapse', style({
+        transform : 'translateX(-100%)'
+      })),
+      state('expand', style({
+        transform : 'translateX(0%)'
+      })),
+      transition('collapse => expand', animate('200ms ease-out')),
+      transition('expand => collapse', animate('1000ms ease-in'))
+  ]),
+  ]
 })
 export class AppComponent {
   title = 'app';
+  // tslint:disable-next-line:no-inferrable-types
+  state: string = 'expand';
+
+  onClick() {
+    this.state = (this.state === 'expand' ? 'collapse' : 'expand');
+  }
 }
